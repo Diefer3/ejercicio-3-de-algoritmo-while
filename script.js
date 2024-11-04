256141aedb9fd7c7f73e2608736d8c2ab262b4b0
Diego <creacioneszaide@gmail.com>
2024-11-04 18:13:01 -0500

    ingreso el ciclo do while y sus parametros

--- script.js
+++ script.js
@@ -5,7 +5,15 @@
 let acumulado = 0; // cree las variables donde ingreso el valor a almacenar
 let numero;
 
+do { 
+    nuemro = parseInt(pormot("ingresa un numnero para sumar(o ingresa 0000 para ternimar"));
+    // ingresando la varible el numero, tambien coloco que salga un cuadro de dialogo 
+    // y que me devuelven el valor en numero
 
+    if(numero !== 0000){
+        acumulado += numero;
+    }
+}
 
 
 
