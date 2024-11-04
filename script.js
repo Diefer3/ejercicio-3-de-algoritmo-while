222782e2082f0588f4033a814c921693110f6c01
Diego <creacioneszaide@gmail.com>
2024-11-04 18:17:32 -0500

    guardar las salida

--- script.js
+++ script.js
@@ -6,14 +6,35 @@ let acumulado = 0; // cree las variables donde ingreso el valor a almacenar
 let numero;
 
 do { 
-    nuemro = parseInt(pormot("ingresa un numnero para sumar(o ingresa 0000 para ternimar"));
-    // ingresando la varible el numero, tambien coloco que salga un cuadro de dialogo 
+    numero = parseInt(prompt("ingresa un numnero para sumar(o ingresa 0000 para ternimar"));
+    // ingresando la varible numero, tambien coloco que salga un cuadro de dialogo 
     // y que me devuelven el valor en numero
 
-    if(numero !== 0000){
+    if (numero !== 0000) {
         acumulado += numero;
     }
+} while (numero !== 0000); 
+
+document.write("el valor acumulado es:" + acumulado + "<br>");
+//salida por document.write
+
+if (acumulado === 0){
+    document.write("el valor acumulado es cero");
+
+} else if (acumulado > 0) {
+    document.write("el valor acumulado es mayor a cero");
+
+
+}else { 
+    document.write("el valor acumulado es menor a cero");
+    
 }
+
+
+
+
+
+
 
 
 
