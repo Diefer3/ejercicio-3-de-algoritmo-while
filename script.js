a9cddc8b3c41956c25cdf2757368fd2cb0db84ca
Diego <creacioneszaide@gmail.com>
2024-11-04 17:35:09 -0500

    creo las varibles para ingresar los datos

---
+++ script.js
@@ -0,0 +1,11 @@
+// La sentencia do...while en programación es una estructura de control de flujo que se utiliza para ejecutar un bloque de código al menos una vez y, 
+//después de eso, repetir la ejecución del bloque mientras se cumpla una condición especificada.
+// si la represntamos por medio de un ejercicio
+
+let acumulado = 0; // cree las variables donde ingreso el valor a almacenar
+let numero;
+
+
+
+
+
